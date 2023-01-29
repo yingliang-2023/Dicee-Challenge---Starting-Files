79e81a9462f1a03afdcab6328d7456ddd5f2c72c
YING LIANG <y.liang.168@westcliff.edu>
2023-01-29 15:41:40 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+var randomNumber1=Math.floor(Math.random()*6)+1;
+
+
+document.querySelector(.img1).setAttribute("src","'images/dice'+randomNumber1+'.png'");
+
+document.querySelector(.img2).setAttribute("src","'images/dice'+randomNumber1+'.png'");
+
